8200a7eb77959e273fe2280e8739cc5d7d3d8d5a
Blasty25 <sohamshah210@gmail.com>
2025-04-09 19:24:51 -0400

    Finished adding contoller bindings, ALL PID + FF values are = to 0. Change these values or your elevator will NOT move in Sim. Try to get a perfect PID graph like the one on the presentation

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,13 +6,35 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.elevator.Elevator;
+import frc.robot.elevator.ElevatorConstants;
+import frc.robot.elevator.ElevatorIOReal;
+import frc.robot.elevator.ElevatorIOSim;
 
 public class RobotContainer {
+  public Elevator elevator;
+  private CommandXboxController controller = new CommandXboxController(0);
+
   public RobotContainer() {
+    if (Robot.isReal()) {
+      elevator = new Elevator(new ElevatorIOReal());
+    } else {
+      elevator = new Elevator(new ElevatorIOSim());
+    }
     configureBindings();
   }
 
-  private void configureBindings() {}
+  private void configureBindings() {
+
+    controller.y().onTrue(elevator.runSetpoint(ElevatorConstants.l4));
+    controller.x().onTrue(elevator.runSetpoint(ElevatorConstants.l3));
+    controller.b().onTrue(elevator.runSetpoint(ElevatorConstants.l2));
+    controller.a().onTrue(elevator.runSetpoint(ElevatorConstants.l1));
+    controller.povDown().onTrue(elevator.runSetpoint(ElevatorConstants.stow));
+
+    controller.povUp().onTrue(elevator.resetEncoder());
+  }
 
   public Command getAutonomousCommand() {
     return Commands.print("No autonomous command configured");
