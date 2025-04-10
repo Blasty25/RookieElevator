740390d2fc07ec93bbba5578a8080d77577c9b78
Blasty25 <sohamshah210@gmail.com>
2025-04-09 20:17:29 -0400

    Changed Real implemntation to a learning implemntaiton

--- src/main/java/frc/robot/elevator/ElevatorIOReal.java
+++ src/main/java/frc/robot/elevator/ElevatorIOReal.java
@@ -10,65 +10,51 @@ import static edu.wpi.first.units.Units.MetersPerSecond;
 import static edu.wpi.first.units.Units.Volts;
 
 import com.revrobotics.RelativeEncoder;
-import com.revrobotics.spark.SparkBase.PersistMode;
-import com.revrobotics.spark.SparkBase.ResetMode;
-import com.revrobotics.spark.SparkLowLevel.MotorType;
-import com.revrobotics.spark.SparkMax;
-import com.revrobotics.spark.config.ClosedLoopConfig.FeedbackSensor;
-import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
-import com.revrobotics.spark.config.SparkMaxConfig;
-import org.littletonrobotics.junction.Logger;
 
-/** Add your docs here. */
+/* This is the explanation on what to do here
+ * Make a instance of the SparkMax Config Class like so: SparkMaxConfig config = new SparkMaxConfig;
+ * After that in the Constructor(The constructor runs once in each class(often on intialization) public ElevatorIOReal(){} is a constructor)
+ * You need to do config.idleMode(coast); This will make the motors move easier.
+ * You can look throught the different configs to see what is the best config for a elevator
+ * If you have any questions about any of the configs ask about them in the programming channel.
+ */
+
 public class ElevatorIOReal implements ElevatorIO {
-  private SparkMaxConfig config = new SparkMaxConfig();
-
-  private SparkMax leftSparky = new SparkMax(ElevatorConstants.leftID, MotorType.kBrushless);
-  private SparkMax rightSparky = new SparkMax(ElevatorConstants.rightID, MotorType.kBrushless);
 
   public RelativeEncoder encoder;
-
   public ElevatorIOReal() {
-    config.idleMode(IdleMode.kCoast).inverted(false).voltageCompensation(12);
 
-    config
-        .encoder
-        .positionConversionFactor(ElevatorConstants.positionConversionFactor)
-        .velocityConversionFactor(ElevatorConstants.positionConversionFactor / 60);
+    // config
+    //     .encoder
+    //     .positionConversionFactor(ElevatorConstants.positionConversionFactor)
+    //     .velocityConversionFactor(ElevatorConstants.positionConversionFactor / 60);
 
-    config.closedLoop.feedbackSensor(FeedbackSensor.kPrimaryEncoder);
 
-    leftSparky.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
-
-    config.follow(leftSparky, true);
-
-    rightSparky.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
-
-    encoder = leftSparky.getEncoder();
-    encoder.setPosition(0.0);
+    // encoder = leftSparky.getEncoder();
+    // encoder.setPosition(0.0);
   }
 
   @Override
   public void updateInputs(ElevatorIOInputs inputs) {
-    inputs.currentHeight = Meters.ofBaseUnits(encoder.getPosition());
+    // inputs.currentHeight = Meters.ofBaseUnits(encoder.getPosition());
 
-    inputs.leftCurrent = Amps.of(leftSparky.getOutputCurrent());
-    inputs.rightCurrent = Amps.of(rightSparky.getOutputCurrent());
+    // inputs.leftCurrent = Amps.of(leftSparky.getOutputCurrent());
+    // inputs.rightCurrent = Amps.of(rightSparky.getOutputCurrent());
 
-    inputs.leftVolts = Volts.of(leftSparky.getAppliedOutput() * leftSparky.getBusVoltage());
-    inputs.rightVolts = Volts.of(rightSparky.getAppliedOutput() * rightSparky.getBusVoltage());
+    // inputs.leftVolts = Volts.of(leftSparky.getAppliedOutput() * leftSparky.getBusVoltage());
+    // inputs.rightVolts = Volts.of(rightSparky.getAppliedOutput() * rightSparky.getBusVoltage());
 
-    inputs.velocity = MetersPerSecond.of(encoder.getVelocity());
-    Logger.recordOutput("Elevator/RawRotations", leftSparky.getEncoder().getPosition());
+    // inputs.velocity = MetersPerSecond.of(encoder.getVelocity());
+    // Logger.recordOutput("Elevator/RawRotations", leftSparky.getEncoder().getPosition());
   }
 
   @Override
   public void setVolts(double volts) {
-    leftSparky.setVoltage(volts);
+    // leftSparky.setVoltage(volts);
   }
 
   @Override
   public void resetEncoder() {
-    encoder.setPosition(0.0);
+    // encoder.setPosition(0.0);
   }
 }
